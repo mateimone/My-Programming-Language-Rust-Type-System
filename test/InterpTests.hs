9a6091adca7943951f3b892768996419fbd35e29
mateimone <m.t.ivan-1@student.tudelft.nl>
2025-05-21 20:04:32 +0200

    weird problem with tests?

--- test/InterpTests.hs
+++ test/InterpTests.hs
@@ -8,23 +8,26 @@ import Value
 interpTest :: String -> Value -> Spec
 interpTest input expected =
     it (input ++ " should be " ++ show expected) $ do
-            run input `shouldBe` Right expected
+            result <- run input
+            result `shouldBe` Right expected
 
 test :: IO ()
 test = hspec $ do
     describe "Interpreter: good weather tests" $ do
-        interpTest "let x = 2 in x + 3" (VInt 5)
-        interpTest "True && False" (VBool False)
+       -- interpTest "let x = 2 in x + 3" (VInt 5)
+       interpTest "True && False" (VBool False)
 
-        interpTest "val x = 3; x * x" (VInt 9)
-        interpTest "fun square (x : int) = x * x; square 3" (VInt 9)
+       interpTest "val x = 3; x * x" (VInt 9)
+       interpTest "fun square (x : int) -> int = {x * x} square(3)" (VInt 9)
 
     describe "Interpreter: order of operation" $ do
-        it "multiplication should come before addition" $ do
-            let result = run "2 + 3 * 4"
-            result `shouldBe` Right (VInt 14)
-            run "4 * 3 + 2" `shouldBe` result
+       it "multiplication should come before addition" $ do
+              result <- run "2 + 3 * 4"
+              result `shouldBe` Right (VInt 14)
+              result2 <- run "4 * 3 + 2"
+              result2 `shouldBe` result
 
-        it "brackets should have the highest priority" $ do
-            run "(2 + 3) * 4" `shouldBe` Right (VInt 20)
+       it "brackets should have the highest priority" $ do
+              result <- run "(2 + 3) * 4"
+              result `shouldBe` Right (VInt 20)
 
--- test/TypeCheckTests.hs
+++ test/TypeCheckTests.hs
@@ -9,25 +9,28 @@ import Data.Either ( isLeft )
 
 tcTest :: String -> Type -> Spec
 tcTest input expected =
-    it (input ++ " should type check to " ++ show expected) $ do
-            infertype input `shouldBe` Right expected
+   it (input ++ " should type check to " ++ show expected) $ do
+         res <- infertype input
+         res `shouldBe` Right expected
 
 tcErrorTest :: String -> Spec
 tcErrorTest input =
-    it (input ++ " should not type check") $ do
-            infertype input `shouldSatisfy` isLeft
+   it (input ++ " should not type check") $ do
+         res <- infertype input
+         res `shouldSatisfy` isLeft
 
 test :: IO ()
 test = hspec $ do
-    describe "typeChecker: good weather tests" $ do
-        tcTest "let x = 2 in x + 3" TInt
-        tcTest "True && False" TBool
+   describe "typeChecker: good weather tests" $ do
+      -- tcTest "let x = 2 in x + 3" TInt
+      tcTest "True && False" TBool
 
-        tcTest "val x = 3; x * x" TInt
+      tcTest "val x = 3; x * x" TInt
 
-        tcTest "fun isZero (x : int) = x == 0; isZero 42" TBool
+      tcTest "fun isZero (x : int) -> bool = {x == 0} isZero(42)" TBool
 
-    describe "typeChecker: bad weather tests" $ do
-        tcErrorTest "let x = 2 in x + True"
-        tcErrorTest "True && 3"
-        tcErrorTest "fun increment (x : int) = x + 1; increment True"
+   describe "typeChecker: bad weather tests" $ do
+      -- tcErrorTest "let x = 2 in x + True"
+      tcErrorTest "True && 3"
+      tcErrorTest "fun increment (x : int) -> int = {x + 1} val b = True; increment(b)"
+
