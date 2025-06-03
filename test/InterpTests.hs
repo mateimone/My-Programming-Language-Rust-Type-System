a6fbf66ea13655f84faec8f0043d8cd87332a6a5
mateimone <m.t.ivan-1@student.tudelft.nl>
2025-06-03 16:13:39 +0200

    Tests for phase 0, 1, 2

--- test/InterpTests.hs
+++ test/InterpTests.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 
 import Run ( run )
 import Value
+import Data.Either ( isLeft )
 
 interpTest :: String -> Value -> Spec
 interpTest input expected =
@@ -11,23 +12,264 @@ interpTest input expected =
             result <- run input
             result `shouldBe` Right expected
 
+interpErrorTest :: String -> Spec
+interpErrorTest input =
+    it (input ++ " should not interpret ") $ do
+         res <- run input
+         res `shouldSatisfy` isLeft
+
 test :: IO ()
 test = hspec $ do
-    describe "Interpreter: good weather tests" $ do
-       -- interpTest "let x = 2 in x + 3" (VInt 5)
-       interpTest "True && False" (VBool False)
+   describe "Interpreter: good weather tests" $ do
+   -- interpTest "let x = 2 in x + 3" (VInt 5)
+      interpTest "return (True && False)" (VBool False)
 
-       interpTest "val x = 3; x * x" (VInt 9)
-       interpTest "fun square (x : int) -> int = {x * x} square(3)" (VInt 9)
+      interpTest "val x = 3; return (x * x)" (VInt 9)
+      interpTest "fun square (x : int) -> int = {return (x * x)} return square(3)" (VInt 9)
+      interpTest "val mut x = 2; fun square (x : int) -> int = {return (x * x)} val z = square(x); return x" (VInt 2)
 
-    describe "Interpreter: order of operation" $ do
-       it "multiplication should come before addition" $ do
-              result <- run "2 + 3 * 4"
-              result `shouldBe` Right (VInt 14)
-              result2 <- run "4 * 3 + 2"
-              result2 `shouldBe` result
+   describe "Interpreter: order of operation" $ do
+      it "multiplication should come before addition" $ do
+            result <- run "return (2 + 3 * 4)"
+            result `shouldBe` Right (VInt 14)
+            result2 <- run "return (4 * 3 + 2)"
+            result2 `shouldBe` result
 
-       it "brackets should have the highest priority" $ do
-              result <- run "(2 + 3) * 4"
-              result `shouldBe` Right (VInt 20)
+      it "brackets should have the highest priority" $ do
+            result <- run "return ((2 + 3) * 4)"
+            result `shouldBe` Right (VInt 20)
+
+   describe "Interpreter: Primitive copy tests" $ do
+      interpTest  ("val a = 1;" ++
+               "val b = a;" ++
+               "val c = a;" ++
+               "fun test(x:int) -> int = {return x}" ++ 
+               "val a = test(a);" ++
+               "return a"
+            ) (VInt 1)
+
+   describe "typeChecker: list tests" $ do
+      interpTest  ("val list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[1];" ++
+               "return list[2]"
+            ) (VInt 3)
+
+      interpTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[1];" ++
+               "set list[2] = 4;" ++
+               "return list[2]"
+            ) (VInt 4)
+
+      interpTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "list.insert(3, 4);" ++
+               "list.insert(0, 5);" ++
+               "return ((list[4] == 4) && (list[0] == 5))"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "list.push(4);" ++
+               "return list[3]"
+            ) (VInt 4)
+
+      interpTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "val p = list.remove(1);" ++
+               "return list[1]"
+            ) (VInt 3)
+
+      interpTest  ("val mut list = vec![Green];" ++
+                    "val a = list.remove(0);" ++ 
+                    "return a"
+               ) (VLight Green)
+
+      interpTest  ("val mut list = vec![Green];" ++
+                   "return list.remove(0)" 
+               ) (VLight Green)
+
+      interpTest  ("val mut list = vec![Green, Red];" ++
+                   "return ((list.remove(0) == Green) && (list.remove(0) == Red))"
+               ) (VBool True)
+
+      interpTest  ("val list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "return ((a == 1) && (list[0][0][2] == 3))"
+            ) (VBool True)
+      
+      interpTest  ("val list:List<List<List<int>>> = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "return ((a == 1) && (list[0][0][2] == 3))"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "val x = (list[0][0]).remove(1);" ++ 
+               "return ((a == 1) && (x == 2) && ((list[0][0]).remove(1) == 3))"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = (list[0]).remove(0);" ++ 
+               "return (a[1] == 2)"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = (list[0][0]).remove(1);" ++ 
+               "return (a == 2)"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "(list[0]).push(vec![6, 7, 8]);" ++ 
+               "return (list[0][1][2] == 8)"
+            ) (VBool True)
+
+      interpTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "(list[0][0]).insert(0, 4);" ++ 
+               "return ((list[0][0][0] == 4) && (list[0][0][1] == 1) && (list[0][0][2] == 2) && (list[0][0][3] == 3))"
+            ) (VBool True)
+
+      interpErrorTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0];" ++ 
+               "return -1"
+            ) 
+      
+      it "Out of bounds" $ do
+         a <- run  ("val list = vec![1];" ++
+                     "return list[1]"
+                  ) 
+         a `shouldSatisfy` isLeft
+
+         b <- run ("val list = vec![1];" ++
+                    "set list[0] = 3;" ++ 
+                    "return 0"
+               )
+         b `shouldSatisfy` isLeft
+
+         c <- run ("val mut list = vec![1];" ++
+                    "val a = list.remove(0);" ++ 
+                    "return list[0]"
+               )
+         c `shouldSatisfy` isLeft
+
+      interpErrorTest  ("val mut list = vec![1];" ++
+                    "val a = list.remove(1);" ++ 
+                    "return 0"
+               )
+
+      interpErrorTest  ("val mut list = vec![1];" ++
+                    "set list[1] = 3;" ++ 
+                    "return 0"
+               )
+
+      interpErrorTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "list.insert(4, 4);" ++
+               "return list[3]"
+               )
+
+   describe "Interpreter: Light tests (for moving)" $ do
+      interpTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = Yellow;" ++
+               "ass lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val lamp1 = Green;" ++ 
+               "val lamp2 = lamp1;" ++
+               "val lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val lamp1 = Green;" ++ 
+               "val lamp2 = lamp1;" ++
+               "val lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "val lamp1 = Yellow;" ++ 
+               "ass lamp2 = lamp1;" ++
+               "return lamp2"
+            ) (VLight Yellow)
+
+      interpTest  ("val mut lamp1 = Green;" ++ 
+               "fun test(mut l:Light) -> Light = {return l}" ++
+               "val lamp1 = test(lamp1);" ++ 
+               "return lamp1"
+            ) (VLight Green)
+
+      interpTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = Yellow;" ++
+               "ass lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val lamp1 = Green;" ++ 
+               "val lamp2 = lamp1;" ++
+               "val lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "return lamp2"
+            ) (VLight Green)
+
+      interpTest  ("val lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "val lamp1 = Yellow;" ++ 
+               "ass lamp2 = lamp1;" ++
+               "return lamp2"
+            ) (VLight Yellow)
+
+      interpTest  ("val lamp1 = Red;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp2 = Green;" ++ 
+               "val lamp3 = lamp2;" ++ 
+               "return lamp3"
+            ) (VLight Green)
+
+      interpTest  ("fun immutable(l:Light) -> Light = {return Red}" ++
+               "fun mutable(mut l:Light) -> Light = {return Red}" ++ 
+               "val imm_light = Red;" ++ 
+               "val z = immutable(imm_light);" ++
+               "val imm_light = Red;" ++ 
+               "val zz = mutable(imm_light);" ++ 
+               "val mut mut_light = Yellow;" ++
+               "val zzz = immutable(mut_light);" ++ 
+               "val mut mut_light = Yellow;" ++ 
+               "val zzzz = mutable(mut_light);" ++ 
+               "return zzzz"
+            ) (VLight Red)
+
+   
 
--- test/TypeCheckTests.hs
+++ test/TypeCheckTests.hs
@@ -23,14 +23,317 @@ test :: IO ()
 test = hspec $ do
    describe "typeChecker: good weather tests" $ do
       -- tcTest "let x = 2 in x + 3" TInt
-      tcTest "True && False" TBool
+      tcTest "return (True && False)" TBool
 
-      tcTest "val x = 3; x * x" TInt
+      tcTest "val x = 3; return (x * x)" TInt
 
-      tcTest "fun isZero (x : int) -> bool = {x == 0} isZero(42)" TBool
+      tcTest "fun isZero (x : int) -> bool = {return x == 0} return isZero(42)" TBool
 
    describe "typeChecker: bad weather tests" $ do
       -- tcErrorTest "let x = 2 in x + True"
       tcErrorTest "True && 3"
-      tcErrorTest "fun increment (x : int) -> int = {x + 1} val b = True; increment(b)"
+      tcErrorTest "fun increment (x : int) -> int = {return x + 1} val b = True; return increment(b)"
+
+   describe "typeChecker: list tests" $ do
+      tcTest  ("val list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[1];" ++
+               "return list[2]"
+            ) TInt
+
+      tcTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[1];" ++
+               "set list[2] = 4;" ++
+               "return list[2]"
+            ) TInt
+
+      tcTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "list.insert(3, 4);" ++
+               "return list[3]"
+            ) TInt
+
+      tcTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "list.push(4);" ++
+               "return list[3]"
+            ) TInt
+
+      tcTest  ("val mut list = vec![1,2,3];" ++
+               "val a = list[0];" ++ 
+               "val b = list[0];" ++
+               "val p = list.remove(1);" ++
+               "return list[1]"
+            ) TInt
+
+      tcTest  ("val mut list = vec![Green];" ++
+                    "val a = list.remove(0);" ++ 
+                    "return a"
+               ) TLight
+
+      tcTest  ("val mut list = vec![Green];" ++
+               "return list.remove(0)" 
+               ) TLight
+
+      tcTest  ("val mut list = vec![vec![vec![1]]];" ++
+               "return list" 
+               ) (TList (TList (TList TInt)))
+
+      tcTest  ("val mut list:List<Light> = vec![Green];" ++ 
+               "val a:Light = Red;" ++ 
+               "list.insert(0, a);" ++ 
+               "return 0"
+            ) TInt
+
+      tcTest  ("val list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "return ((a == 1) && (list[0][0][2] == 3))"
+            ) TBool
+      
+      tcTest  ("val list:List<List<List<int>>> = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "return ((a == 1) && (list[0][0][2] == 3))"
+            ) TBool
+
+      tcTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0][0];" ++ 
+               "val x = (list[0][0]).remove(1);" ++ 
+               "return ((a == 1) && (x == 2) && ((list[0][0]).remove(1) == 3))"
+            ) TBool
+
+      tcTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = (list[0]).remove(0);" ++ 
+               "return (a[1] == 2)"
+            ) TBool
+
+      tcTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = (list[0][0]).remove(1);" ++ 
+               "return (a == 2)"
+            ) TBool
+
+      tcTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "(list[0]).push(vec![6, 7, 8]);" ++ 
+               "return (list[0][1][2] == 8)"
+            ) TBool
+
+      tcTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "(list[0][0]).insert(0, 4);" ++ 
+               "return ((list[0][0][0] == 4) && (list[0][0][1] == 1) && (list[0][0][2] == 2) && (list[0][0][3] == 3))"
+            ) TBool
+
+      tcErrorTest  ("val mut list = vec![vec![vec![1,2,3]]];" ++ 
+               "val a = list[0][0];" ++ 
+               "return -1"
+            ) 
+            
+      tcErrorTest  ("val list = vec![1];" ++
+                    "set list[0] = 3;" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val list = vec![1];" ++
+                    "list.insert(0, 2);" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val list = vec![1];" ++
+                    "list.push(2);" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val list = vec![1];" ++
+                    "set list[0] = 3;" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val mut list = vec![Green];" ++
+                    "val a = list[0];" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val mut list = vec![Green];" ++
+                    "list.insert(0, 2);" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val mut list = vec![Green];" ++
+                    "list.push(1);" ++ 
+                    "return 0"
+               )
+
+      tcErrorTest  ("val mut list:List<Light> = vec![1];" ++
+                    "return 0"
+               )
+
+      tcErrorTest  ("val mut list = vec![Green];" ++
+                    "val a: int = list[0];" ++ 
+                    "return 0"
+               )
+      
+      tcErrorTest  ("val list = vec![Green];" ++
+                    "return list[0]"
+               )
+
+      tcErrorTest  ("val mut list = vec![Green, Red];" ++
+                   "fun test(list:List<Light>) -> Light = {val o = list.remove(0); val o = list.remove(0); return Green}" ++
+                   "val z = test(list)" ++
+                   "return ((list.remove(0) == Green) && (list.remove(0) == Red))"
+               )
+      
+      tcErrorTest  ("val mut list = vec![0, 1];" ++
+                   "fun test(list:List<int>) -> Light = {val o = list.remove(0); val o = list.remove(0); return Green}" ++
+                   "val z = test(list)" ++
+                   "return ((list.remove(0) == 0) && (list.remove(0) == 1))"
+               ) 
+
+   describe "typeChecker: primitive copy tests" $ do
+      tcTest  ("val a = 1;" ++
+               "val b = a;" ++
+               "val c = a;" ++
+               "fun test(x:int) -> int = {return x}" ++ 
+               "val a = test(a);" ++
+               "return a"
+            ) TInt
+   
+   describe "typeChecker: light tests (for moving)" $ do
+      tcErrorTest ("val lamp1 = Green;" ++ 
+             "val lamp2 = Yellow;" ++
+             "fun test(lamp1:Light, lamp2:Light) -> bool = {" ++
+             "return lamp1 == lamp2" ++
+             "}" ++ 
+             "val b1 = test(lamp1, lamp2);" ++ 
+             "val b2 = test(lamp1, lamp2);" ++ 
+             "return b2")
+      
+      tcErrorTest ("val lamp1 = Green;" ++ 
+                   "val lamp2 = lamp1;" ++
+                   "val lamp3 = lamp1;" ++ 
+                   "return lamp3"
+            )
+
+      tcErrorTest  ("val mut lamp2 = Red;" ++
+                    "fun test(l:Light) -> Light = {return Green}" ++
+                    "val u2 = test(lamp2);" ++ 
+                    "return lamp2"
+            ) 
+
+      tcTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = Yellow;" ++
+               "ass lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val lamp1 = Green;" ++ 
+               "val lamp2 = lamp1;" ++
+               "val lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "val lamp1 = Yellow;" ++ 
+               "ass lamp2 = lamp1;" ++
+               "return lamp2"
+            ) TLight
+
+      tcTest  ("val mut lamp1 = Green;" ++ 
+               "fun test(mut l:Light) -> Light = {return l}" ++
+               "val lamp1 = test(lamp1);" ++ 
+               "return lamp1"
+            ) TLight
+
+      tcErrorTest ("val lamp1 = Green;" ++ 
+             "val lamp2 = Yellow;" ++
+             "fun test(lamp1:Light, lamp2:Light) -> bool = {" ++
+             "return lamp1 == lamp2" ++
+             "}" ++ 
+             "val b1 = test(lamp1, lamp2);" ++ 
+             "val b2 = test(lamp1, lamp2);" ++ 
+             "return b2")
+      
+      tcErrorTest ("val lamp1 = Green;" ++ 
+                   "val lamp2 = lamp1;" ++
+                   "val lamp3 = lamp1;" ++ 
+                   "return lamp3"
+            )
+
+      tcErrorTest  ("val mut lamp2 = Red;" ++
+                    "fun test(l:Light) -> Light = {return Green}" ++
+                    "val u2 = test(lamp2);" ++ 
+                    "return lamp2"
+            ) 
+
+      tcTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = Yellow;" ++
+               "ass lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val mut lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val lamp1 = Green;" ++ 
+               "val lamp2 = lamp1;" ++
+               "val lamp1 = Red;" ++
+               "fun test(l:Light) -> Light = {return Green}" ++
+               "val u1 = test(lamp1);" ++ 
+               "val u2 = test(lamp2);" ++ 
+               "return u2"
+            ) TLight
+
+      tcTest  ("val lamp1 = Green;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "val lamp1 = Yellow;" ++ 
+               "ass lamp2 = lamp1;" ++
+               "return lamp2"
+            ) TLight
+
+      tcTest  ("val lamp1 = Red;" ++ 
+               "val mut lamp2 = lamp1;" ++
+               "ass lamp2 = Green;" ++ 
+               "val lamp3 = lamp2;" ++ 
+               "return lamp3"
+            ) TLight
+
+      tcTest  ("fun immutable(l:Light) -> Light = {return Red}" ++
+               "fun mutable(mut l:Light) -> Light = {return Red}" ++ 
+               "val imm_light = Red;" ++ 
+               "val z = immutable(imm_light);" ++
+               "val imm_light = Red;" ++ 
+               "val zz = mutable(imm_light);" ++ 
+               "val mut mut_light = Yellow;" ++
+               "val zzz = immutable(mut_light);" ++ 
+               "val mut mut_light = Yellow;" ++ 
+               "val zzzz = mutable(mut_light);" ++ 
+               "return Red"
+            ) TLight
 
